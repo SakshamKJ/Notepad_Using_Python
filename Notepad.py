313560e8803183c2e5be3df4e85717e7f75883eb
SAKSHAM KUMAR JHA <72131625+SakshamKJ@users.noreply.github.com>
2022-02-05 18:14:09 +0530

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -8,7 +8,6 @@ class Notepad:
 
 	__root = Tk()
 
-	# default window width and height
 	__thisWidth = 300
 	__thisHeight = 300
 	__thisTextArea = Text(__root)
@@ -17,19 +16,16 @@ class Notepad:
 	__thisEditMenu = Menu(__thisMenuBar, tearoff=0)
 	__thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
 	
-	# To add scrollbar
 	__thisScrollBar = Scrollbar(__thisTextArea)	
 	__file = None
 
 	def __init__(self,**kwargs):
 
-		# Set icon
 		try:
 				self.__root.wm_iconbitmap("Notepad.ico")
 		except:
 				pass
 
-		# Set window size (the default is 300x300)
 
 		try:
 			self.__thisWidth = kwargs['width']
@@ -41,40 +37,30 @@ class Notepad:
 		except KeyError:
 			pass
 
-		# Set the window text
 		self.__root.title("Untitled - Notepad")
 
-		# Center the window
 		screenWidth = self.__root.winfo_screenwidth()
 		screenHeight = self.__root.winfo_screenheight()
 	
-		# For left-alling
 		left = (screenWidth / 2) - (self.__thisWidth / 2)
 		
-		# For right-allign
 		top = (screenHeight / 2) - (self.__thisHeight /2)
 		
-		# For top and bottom
 		self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth,
 											self.__thisHeight,
 											left, top))
 
-		# To make the textarea auto resizable
 		self.__root.grid_rowconfigure(0, weight=1)
 		self.__root.grid_columnconfigure(0, weight=1)
-
-		# Add controls (widget)
+		
 		self.__thisTextArea.grid(sticky = N + E + S + W)
 		
-		# To open new file
 		self.__thisFileMenu.add_command(label="New",
 										command=self.__newFile)	
 		
-		# To open a already existing file
 		self.__thisFileMenu.add_command(label="Open",
 										command=self.__openFile)
 		
-		# To save current file
 		self.__thisFileMenu.add_command(label="Save",
 										command=self.__saveFile)	
 
